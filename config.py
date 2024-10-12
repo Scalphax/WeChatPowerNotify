5dc75a39523b1b2a0b4afb8c2fe42e272d3c9985
Scalphax <scalphax@qiulishe.com>
2024-10-12 18:47:11 +0800

    fix login module

--- config.py
+++ config.py
@@ -1,22 +1,43 @@
-import sys,requests,rsa,base64
+import requests,rsa,base64,json
 from bs4 import BeautifulSoup as bs
+from urllib3.exceptions import InsecureRequestWarning
+
+# 禁用 InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 token = "AT_Ij1vvIn0agXc1xlgRvaepYbWK92QWbyp"
 Username = "1024007793"
 Password = "sand0323"
 
-def get(method:str,up_dict):   #getAreaInfo,queryBuildList,queryFloorList,getRoomInfo,queryRoomElec,queryReserve
-    url = f"http://cwsf.whut.edu.cn/{method}"
-    response = requests.post(url, data=up_dict, headers=headers)
+def get(method: str, up_dict):  # getAreaInfo, queryBuildList, queryFloorList, getRoomInfo, queryRoomElec, queryReserve
+    global jsessionid
     try:
-        return response.json()
-    except:
-        print("session过期")
-        global jsessionid
-        jsessionid = get_session()
         cookie = f"JSESSIONID={jsessionid}"
-        headers = {"Proxy-Connection":"keep-alive","X-Requested-With":"XMLHttpRequest","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36","Accept":"application/json, text/javascript, */*; q=0.01","Content-Type":"application/x-www-form-urlencoded; charset=UTF-8","Origin":"http://cwsf.whut.edu.cn","Referer":"http://cwsf.whut.edu.cn/nyyPayElecPages51274E035","Accept-Encoding":"gzip, deflate","Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6","Cookie":cookie}
-        get(method,up_dict)
+    except:
+        jsessionid = get_session()
+        return get(method, up_dict)  # 确保返回递归调用的结果
+    else:
+        headers = {
+            "Proxy-Connection": "keep-alive",
+            "X-Requested-With": "XMLHttpRequest",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": "http://cwsf.whut.edu.cn",
+            "Referer": "http://cwsf.whut.edu.cn/nyyPayElecPages51274E035",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6",
+            "Cookie": cookie
+        }
+        url = f"http://cwsf.whut.edu.cn/{method}"
+        response = requests.post(url, data=up_dict, headers=headers)
+        try:
+            return_data = response.json()
+            return return_data
+        except:
+            print("session过期")
+            jsessionid = get_session()
+            return get(method, up_dict)  
 
 def get_session():
     session = requests.Session()
@@ -46,4 +67,6 @@ def get_session():
 
     session.post('https://zhlgd.whut.edu.cn/tpass/login?service=http%3A%2F%2Fcwsf.whut.edu.cn%2FcasLogin', data=data, verify=False)
     jsessionid = session.cookies.get_dict()['JSESSIONID']
+    print('获取jsessionid成功')
     return jsessionid
+
