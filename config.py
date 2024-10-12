6ac7bc547df745fd74a88445b23bc896ab310786
Scalphax <scalphax@qiulishe.com>
2024-10-12 12:56:20 +0800

    add login function

--- config.py
+++ config.py
@@ -1,22 +1,49 @@
-import urllib
-import json,sys
+import sys,requests,rsa,base64
+from bs4 import BeautifulSoup as bs
 
 token = "AT_Ij1vvIn0agXc1xlgRvaepYbWK92QWbyp"
-jsessionid = "72651FBEBD180CE8563C8B5CA53C9287"
+Username = "1024007793"
+Password = "sand0323"
 
 def get(method:str,up_dict):   #getAreaInfo,queryBuildList,queryFloorList,getRoomInfo,queryRoomElec,queryReserve
     url = f"http://cwsf.whut.edu.cn/{method}"
-    data = urllib.parse.urlencode(up_dict).encode('utf-8')
-    req = urllib.request.Request(url,data=data,headers=headers,method='POST')
+    response = requests.post(url, data=up_dict, headers=headers)
     try:
-        response = urllib.request.urlopen(req).read().decode('utf-8')
-    except:
-        print(f"W:Get 502")
-    try:
-        return json.loads(response)
+        return response.json()
     except:
         print("session过期")
-        sys.exit(0)
+        global jsessionid
+        jsessionid = get_session()
+        cookie = f"JSESSIONID={jsessionid}"
+        headers = {"Proxy-Connection":"keep-alive","X-Requested-With":"XMLHttpRequest","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36","Accept":"application/json, text/javascript, */*; q=0.01","Content-Type":"application/x-www-form-urlencoded; charset=UTF-8","Origin":"http://cwsf.whut.edu.cn","Referer":"http://cwsf.whut.edu.cn/nyyPayElecPages51274E035","Accept-Encoding":"gzip, deflate","Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6","Cookie":cookie}
+        get(method,up_dict)
 
-cookie = f"JSESSIONID={jsessionid}"
-headers = {"Proxy-Connection":"keep-alive","X-Requested-With":"XMLHttpRequest","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36","Accept":"application/json, text/javascript, */*; q=0.01","Content-Type":"application/x-www-form-urlencoded; charset=UTF-8","Origin":"http://cwsf.whut.edu.cn","Referer":"http://cwsf.whut.edu.cn/nyyPayElecPages51274E035","Accept-Encoding":"gzip, deflate","Accept-Language":"zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7,ja;q=0.6","Cookie":cookie}
+def get_session():
+    session = requests.Session()
+    url = "https://zhlgd.whut.edu.cn/tpass/login?service=http%3A%2F%2Fcwsf.whut.edu.cn%2FcasLogin"
+    response = session.get(url, verify=False)
+
+    soup = bs(response.text, 'html.parser')
+
+    lt_value = soup.find('input', {'id': 'lt'}).get('value')
+    public_key_response = session.post('https://zhlgd.whut.edu.cn/tpass/rsa?skipWechat=true', verify=False)
+    public_key_data = public_key_response.json()
+    raw_public_key = public_key_data['publicKey']
+    public_key = f"-----BEGIN PUBLIC KEY-----\n{raw_public_key}\n-----END PUBLIC KEY-----"
+
+    public_key = rsa.PublicKey.load_pkcs1_openssl_pem(public_key.encode())
+    encrypted_u = base64.b64encode(rsa.encrypt(Username.encode(), public_key)).decode()
+    encrypted_p = encrypted_p = base64.b64encode(rsa.encrypt(Password.encode(), public_key)).decode()
+
+    data = {
+        'rsa': '',
+        'ul': encrypted_u,
+        'pl': encrypted_p,
+        'lt': lt_value,
+        'execution': 'e1s1',
+        '_eventId': 'submit'
+    }
+
+    session.post('https://zhlgd.whut.edu.cn/tpass/login?service=http%3A%2F%2Fcwsf.whut.edu.cn%2FcasLogin', data=data, verify=False)
+    jsessionid = session.cookies.get_dict()['JSESSIONID']
+    return jsessionid
