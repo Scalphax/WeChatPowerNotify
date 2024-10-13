ea0147bc057bb756fb57047689780e5a6fbdae6f
Scalphax <scalphax@qiulishe.com>
2024-10-13 16:22:00 +0800

    ...

--- main.py
+++ main.py
@@ -90,6 +90,13 @@ def wrong_code():
     req.post(api_url,json=processed_data,verify=False)
     return '',400
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    return response
+
 @app.route('/')
 def index():
     return render_template('index.html')
